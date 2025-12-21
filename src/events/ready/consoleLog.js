759ec2dce3533f335aa7ea95699f4f32d1093c21
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-12-21 19:58:44 +0500

    ok

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -6,7 +6,7 @@ module.exports = async (client) => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
-    cron.schedule("*/5 * * * *", async () => {
+    cron.schedule("*/4 * * * *", async () => {
       try {
         await assignTweetPointsCron(client);
       } catch (err) {
